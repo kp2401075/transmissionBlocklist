55cb334b09de30fe70248fba6bd96351aa1cd9d7
root <root@localhost.localdomain>
2017-12-23 19:44:03 +1100

    adding bash script

---
+++ getBlocklist.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Download lists, unpack and filter, write to stdout
+curl -s https://www.iblocklist.com/lists.php \
+  | sed -n "s/.*value='\(http:.*=*.*\)'.*/\1/p" \
+  | xargs wget -O - \
+  | gunzip \
+  | cat >> blocklist.gz
